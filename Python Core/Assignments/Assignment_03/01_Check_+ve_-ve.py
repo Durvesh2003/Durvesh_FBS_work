1937ee35d22593d578263d2a96ef04688effa417
Durvesh2003 <kadavedurvesh42@gmail.com>
2026-02-04 14:27:44 +0530

    ass_3

---
+++ Python Core/Assignments/Assignment_03/01_Check_+ve_-ve.py
@@ -0,0 +1,7 @@
+# 1. Write a program to check if the given number is positive or negative.
+
+num = int(input("Enter a number : "))
+if(num > 0):
+    print(f"{num} is a positive integer")
+else:
+    print(f"{num} is a negative integer")
---
+++ Python Core/Assignments/Assignment_03/02_Check_Vowels.py
@@ -0,0 +1,9 @@
+# 2. Write a program to input any alphabet and check whether it is vowel or consonant.
+
+Str1 = input("Enter the Character(Alphabet) :")
+# Str = Str1.lower()
+if (Str1 == 'a' or Str1 == 'e' or Str1 == 'i' or Str1 == 'o' or Str1 == 'u' or Str1 == 'A' or Str1 == 'E' or Str1 == 'I' or Str1 == 'O' or Str1 == 'U' ):
+    print("It is Vowel")
+else :
+    print("It is consonant")
+
